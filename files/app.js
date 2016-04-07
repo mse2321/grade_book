b2247b8e9b984d18a486db0038abf5c7100b4b6a
mse2321 <mse233@gmail.com>
2016-04-07 11:28:10 -0400

    added all needed pages and ngRoute setup

---
+++ files/app.js
@@ -0,0 +1,36 @@
+var demo = angular.module('demo', ['ngRoute']);
+
+	demo.config(['$routeProvider', function($routeProvider){
+        $routeProvider.when('/', {
+          templateUrl: 'intro.html',
+          controller: 'ctrl'
+        })
+        .when('/login', {
+          templateUrl: 'login.html',
+          controller: 'ctrl'
+        })
+        .when('/dashboard', {
+          templateUrl: 'dashboard.html',
+          controller: 'ctrl'
+        })
+        .when('/grades', {
+          templateUrl: 'grades.html',
+          controller: 'ctrl'
+        })
+        .when('/settings', {
+          templateUrl: 'settings.html',
+          controller: 'ctrl'
+        })
+        .when('/import', {
+          templateUrl: 'import.html',
+          controller: 'ctrl'
+        })
+        .otherwise('/error',  {
+          template: '<p>Error - Page Not Found</p>'
+        });
+      }])
+
+
+demo.controller('ctrl', function(){
+	
+});
